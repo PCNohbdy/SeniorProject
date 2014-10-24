1afa646024c1f5941be0675e0114754c2384477c
kille6525 <kille6525@users.noreply.github.com>
2014-10-23 21:16:40 -0500

    Fixed More bugs

---
+++ SeniorProject/Assets/Scene2Script.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scene2Script : MonoBehaviour,
+        IHandle<LoadLevelButtonMessage>,
+        IHandle<LoadInMainScreen>
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        GameEventAggregator.GameMessenger.Subscribe(this);
+        EventAggregatorManager.Publish(new PlaySoundMessage("LevelMusic", true));
+    }
+
+    void OnDestroy()
+    {
+        EventAggregatorManager.Publish(new StopSoundLoopMessage("LevelMusic"));
+        GameEventAggregator.GameMessenger.Unsubscribe(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            EventAggregatorManager.Publish(new LoadInMainScreen());
+        }
+    }
+
+    public void Handle(LoadLevelButtonMessage message)
+    {
+        Application.LoadLevelAdditive(message.LevelToLoad);
+        Destroy(this.gameObject);
+    }
+
+    public void Handle(LoadInMainScreen message)
+    {
+        Destroy(this.gameObject);
+    }
+}
